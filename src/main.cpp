854dee5a58e442a8553a5842158bbae328ac9c1c
Damyan Yanchev <dvyanchev@gmail.com>
2025-11-01 10:54:04 +0200

    Added frame rate

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,26 @@ World world;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
 	world.init();
 
 	while (world.isRunning())
 	{
+		frameStart = SDL_GetTicks();
+
 		world.run();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 	
 	return 0;
